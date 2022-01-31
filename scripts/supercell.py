59669ff9dfe2f81e41712a0d2082eb45f45566aa
Jack Greisman <Jack.Greisman@gmail.com>
2022-01-31 12:51:38 -0500

    Reformat supercell

--- scripts/supercell.py
+++ scripts/supercell.py
@@ -1,14 +1,15 @@
-'''
+"""
 (c) 2020 Jack Greisman
 Based on script by Thomas Holder (2010)
-'''
+"""
 
 from pymol import cmd, cgo, xray
 import numpy as np
 
+
 def cellbasis(angles, edges):
     """
-    For the unit cell with given angles and edge lengths calculate the 
+    For the unit cell with given angles and edge lengths calculate the
     basis transformation (vectors) as a 4x4 matrix
     """
     rad = np.deg2rad(angles)
@@ -16,12 +17,13 @@ def cellbasis(angles, edges):
     basis[0][1] = np.cos(rad[2])
     basis[1][1] = np.sin(rad[2])
     basis[0][2] = np.cos(rad[1])
-    basis[1][2] = (np.cos(rad[0]) - basis[0][1]*basis[0][2])/basis[1][1]
-    basis[2][2] = np.sqrt(1 - basis[0][2]**2 - basis[1][2]**2)
+    basis[1][2] = (np.cos(rad[0]) - basis[0][1] * basis[0][2]) / basis[1][1]
+    basis[2][2] = np.sqrt(1 - basis[0][2] ** 2 - basis[1][2] ** 2)
     edges.append(1.0)
-    return basis * edges # numpy.array multiplication!
+    return basis * edges  # numpy.array multiplication!
 
-def supercell(a=1, b=1, c=1, object=None, color='blue', name='supercell', withmates=1):
+
+def supercell(a=1, b=1, c=1, object=None, color="blue", name="supercell", withmates=1):
     """
     DESCRIPTION
 
@@ -67,26 +69,26 @@ def supercell(a=1, b=1, c=1, object=None, color='blue', name='supercell', withma
     for i in range(int(a)):
         for j in range(int(b)):
             for k in range(int(c)):
-                ts.append([i,j,k])
+                ts.append([i, j, k])
 
     obj = [
-            cgo.BEGIN,
-            cgo.LINES,
-            cgo.COLOR,
+        cgo.BEGIN,
+        cgo.LINES,
+        cgo.COLOR,
     ]
     obj.extend(cmd.get_color_tuple(color))
-        
+
     for t in ts:
-        shift = basis[0:3,0:3] * t
-        shift = shift[:,0] + shift[:,1] + shift[:,2]
-        
+        shift = basis[0:3, 0:3] * t
+        shift = shift[:, 0] + shift[:, 1] + shift[:, 2]
+
         for i in range(3):
-            vi = basis[0:3,i]
+            vi = basis[0:3, i]
             vj = [
-                    np.array([0.,0.,0.]),
-                    basis[0:3,(i+1)%3],
-                    basis[0:3,(i+2)%3],
-                    basis[0:3,(i+1)%3] + basis[0:3,(i+2)%3]
+                np.array([0.0, 0.0, 0.0]),
+                basis[0:3, (i + 1) % 3],
+                basis[0:3, (i + 2) % 3],
+                basis[0:3, (i + 1) % 3] + basis[0:3, (i + 2) % 3],
             ]
             for j in range(4):
                 obj.append(cgo.VERTEX)
@@ -95,32 +97,33 @@ def supercell(a=1, b=1, c=1, object=None, color='blue', name='supercell', withma
                 obj.extend((shift + vj[j] + vi).tolist())
 
             if withmates:
-                symexpcell('m%d%d%d_' % tuple(t), object, *t)
+                symexpcell("m%d%d%d_" % tuple(t), object, *t)
 
     obj.append(cgo.END)
 
     cmd.delete(name)
     cmd.load_cgo(obj, name)
 
-def symexpcell(prefix='mate', object=None, a=0, b=0, c=0):
+
+def symexpcell(prefix="mate", object=None, a=0, b=0, c=0):
     """
     DESCRIPTION
 
     Creates all symmetry-related objects for the specified object that
     occur with their bounding box center within the unit cell.
-    
+
     USAGE
-    
+
     symexpcell prefix, object, [a, b, c]
-    
+
     ARGUMENTS
-    
+
     prefix = string: prefix of new objects
-    
+
     object = string: object for which to create symmetry mates
-    
+
     a, b, c = integer: create neighboring cell {default: 0,0,0}
-    
+
     SEE ALSO
 
     symexp, http://www.pymolwiki.org/index.php/SuperSym
@@ -140,9 +143,9 @@ def symexpcell(prefix='mate', object=None, a=0, b=0, c=0):
     center = sum(np.array(extent)) * 0.5
     center = np.matrix(center.tolist() + [1.0]).T
     center_cell = basis.I * center
-    
-    extra_shift = [[float(i)] for i in (a,b,c)]
-    
+
+    extra_shift = [[float(i)] for i in (a, b, c)]
+
     i = 0
     matrices = xray.sg_sym_to_mat_list(spacegroup)
     for mat in matrices:
@@ -150,19 +153,20 @@ def symexpcell(prefix='mate', object=None, a=0, b=0, c=0):
 
         mat = np.matrix(mat)
         shift = np.floor(mat * center_cell)
-        mat[0:3,3] -= shift[0:3,0]
-        mat[0:3,3] += extra_shift
-        
+        mat[0:3, 3] -= shift[0:3, 0]
+        mat[0:3, 3] += extra_shift
+
         mat = basis * mat * basis.I
         mat_list = list(mat.flat)
-        
-        name = '%s%d' % (prefix, i)
+
+        name = "%s%d" % (prefix, i)
         cmd.create(name, object)
         cmd.transform_object(name, mat_list, 0)
-        cmd.color(i+1, name)
-        
-cmd.extend('symexpcell', symexpcell)
-cmd.extend('supercell', supercell)
+        cmd.color(i + 1, name)
+
+
+cmd.extend("symexpcell", symexpcell)
+cmd.extend("supercell", supercell)
 
 # tab-completion of arguments
-cmd.auto_arg[3]['supercell'] = [ cmd.object_sc, 'object', '']
+cmd.auto_arg[3]["supercell"] = [cmd.object_sc, "object", ""]
